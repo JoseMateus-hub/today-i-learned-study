1c1eba62f9f964e98192726b4e906c1cfbc196b7
José Mateus <josemateus957@gmail.com>
2026-05-12 09:49:55 -0300

    feat: initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+interface HeaderProps {
+    showForm: boolean;
+    onToggleForm: () => void;
+};
+
+export default function Header({ showForm, onToggleForm }: HeaderProps) {
+    return (
+        <>
+          <h1>today i learned</h1>
+          <button onClick={onToggleForm}>{showForm ? "Fechar" : "Novo Fato"}</button>
+        </>
+    );
+}
