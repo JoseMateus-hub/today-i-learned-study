24e490f2db697da1fa3b04b4247d4bab35273fe5
unknown <josecunha0740@digitalcollege.internal>
2026-06-02 11:13:55 -0300

    feat: implementar avaliacao de dados

--- src/components/NewFactForm.tsx
+++ src/components/NewFactForm.tsx
@@ -1,8 +1,10 @@
 import type { RefObject } from "react";
 import React, { useState } from "react";
-import { CATEGORIES
+import { CATEGORIES } from "../constants";
+import { factSchema } from "../schemas";
+import { flattenError } from "zod/v4/core";
 
- } from "../constants";
+
 interface NewFactFormProps {
   inputRef: RefObject<HTMLInputElement>;
 }
@@ -11,14 +13,19 @@ export default function NewFactForm({ inputRef }: NewFactFormProps) {
    const [text, setText] = useState<string>("");
    const [source, setSource] = useState<string>("");
    const [category, setCategory] = useState<string>("");
+   const [error, setError] = useState<Record<string, string[]>>({});
 
    const charsRemaining = 200 - text.length;
 
    function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
         event.preventDefault();
-       
+        const result = factSchema.safeParse({text, source, category});
+        if (!result.success) {
+           setError(flattenError(result.error).fieldErrors);
+          return;
+        };
 
-        console.log({ text, source, category })
+        setError({});
    }
 
    const categoryOptions = CATEGORIES.map((category) => (
@@ -40,19 +47,22 @@ export default function NewFactForm({ inputRef }: NewFactFormProps) {
           maxLength={200}
         />
         <span>{charsRemaining}</span>
-        <input 
-         type="text"
-         placeholder="URL da fonte (https://...)"
-         value={source}
-         onChange={event => setSource(event.target.value)}
+        {error.text?.[0] && <span>{error.text[0]}</span>}
+        <input
+          type="text"
+          placeholder="fonte confi"
+          value={source}
+          onChange={event => setSource(event.target.value)}
         />
-        <select 
-         value={category}
-         onChange={event => setCategory(event.target.value)}
+        {error.source?.[0] && <span>{error.source[0]}</span>}
+        <select
+          value={category}
+          onChange={event => setCategory(event.target.value)}
         >
           <option value="">selecione uma categoria:</option>
           {categoryOptions}
         </select>
+        {error.category?.[0] && <span>{error.category[0]}</span>}
         <button type="submit">Publicar</button>
       </form>
     </>
