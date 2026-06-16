d9a84b9d09301eb9a5ee17d3b81d2c0b53b34155
unknown <josecunha0740@digitalcollege.internal>
2026-06-16 12:06:23 -0300

    style: atualizar layout e estilos da aplicação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,20 @@ interface HeaderProps {
 
 export default function Header({ showForm, onToggleForm }: HeaderProps) {
     return (
-        <>
-        <span>🧠</span>
-          <h1>hoje eu aprendi</h1>
-          <button onClick={onToggleForm}>{showForm ? "Fechar" : "Novo Fato"}</button>
-        </>
+        <header className="flex items-center justify-between px-8 py-6">
+            <div className="flex items-center gap-3">
+                <span className="text-3xl">🧠</span>
+                <h1 className="text-2xl text-white tracking-wide">
+                    Hoje Eu Aprendi
+                </h1>
+            </div>
+
+            <button
+                onClick={onToggleForm}
+                className="btn bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 px-6 py-3 text-white font-bold"
+        >
+                {showForm ? "Fechar" : "Novo Fato"}
+            </button>
+        </header>
     );
 }
